476b4386a9ada9e7abdfce2f6bd6688c84de961a
ss473901 <satoshi.473901.1205@gmail.com>
2021-11-10 01:58:10 +0000

    Add

--- app/controllers/user/funs_controller.rb
+++ app/controllers/user/funs_controller.rb
@@ -1,2 +1,4 @@
 class User::FunsController < ApplicationController
+  def index
+  end
 end
--- app/controllers/user/tasks_controller.rb
+++ app/controllers/user/tasks_controller.rb
@@ -1,2 +1,19 @@
 class User::TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+    @tasks = Task.all
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.user_id = current_user.id
+    task.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:content, :is_done)
+  end
 end
