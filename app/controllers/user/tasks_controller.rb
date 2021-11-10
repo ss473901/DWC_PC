50ccfeadd8c478bcf6ffc36fcf73ee6e33426212
ss473901 <satoshi.473901.1205@gmail.com>
2021-11-10 09:29:09 +0000

    Add User/idea

--- app/controllers/user/tasks_controller.rb
+++ app/controllers/user/tasks_controller.rb
@@ -12,6 +12,24 @@ class User::TasksController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to new_user_task_path(task)
+  end
+
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+
   private
   def task_params
     params.require(:task).permit(:content, :is_done)
